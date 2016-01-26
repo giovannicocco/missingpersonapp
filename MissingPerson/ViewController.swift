64498534012919865a53592e7485f2bbb6b63d56
Giovanni <giovanni.coco@gmail.com>
2016-01-26 15:17:13 -0200

    First commit. Main Storyboard

---
+++ MissingPerson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MissingPerson
+//
+//  Created by Giovanni Brunno Coco on 25/01/16.
+//  Copyright © 2016 Giovanni Brunno Coco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
